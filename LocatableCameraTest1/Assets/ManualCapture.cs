400933d777dcfd270724569412c72c40f371967f
David Jones <David Jones>
2016-04-30 15:07:36 -0700

    Restructured scene to support automatic capturing of images in addition to the user manually triggering captures.

---
+++ LocatableCameraTest1/Assets/ManualCapture.cs
@@ -0,0 +1,36 @@
+﻿// ReSharper disable CheckNamespace
+
+using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class ManualCapture : MonoBehaviour
+{
+    private CameraCapture _cameraCapture;
+    private GestureRecognizer _gestureRecognizer;
+
+    void Start()
+    {
+        _cameraCapture = GetComponent<CameraCapture>();
+
+        _gestureRecognizer = new GestureRecognizer();
+        _gestureRecognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            TakePicture();
+        };
+        _gestureRecognizer.StartCapturingGestures();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            TakePicture();
+        }
+    }
+
+    private void TakePicture()
+    {
+        _cameraCapture.TakePicture();
+    }
+}
