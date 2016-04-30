72cbd493054c4fce1730249376b1aa8a9b7854e2
David Jones <David Jones>
2016-04-30 16:02:39 -0700

    Added support for a new capture mode that creates a textured quad in the scene at the location and orientation of the camera.  Toggle this setting through the VisionManager in the inspector.  TODO: offset the quad properly so that it is constrained to the actual view frustrum.

--- LocatableCameraTest1/Assets/CameraCapture.cs
+++ LocatableCameraTest1/Assets/CameraCapture.cs
@@ -1,17 +1,26 @@
 ﻿// ReSharper disable CheckNamespace
 
 using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.UI;
 using UnityEngine.VR.WSA.Input;
 using UnityEngine.VR.WSA.WebCam;
+using Debug = UnityEngine.Debug;
 
 /// <summary>
 /// Camera script that will initialize the camera system and take pictures with each Tap gesture and Fire1 press
 /// </summary>
 public class CameraCapture : MonoBehaviour
 {
+    public enum CaptureModeType
+    {
+        File,
+        SceneObject
+    }
+
     //TODO: Nonsensical to give knowledge of the Text element to this Behavior, should be abstracted (e.g. piggybacking off Debug.Log)
     public Text DebugText;
     [Tooltip("Enable automatic capturing of camera images.")]
@@ -19,11 +28,14 @@ public class CameraCapture : MonoBehaviour
     [Tooltip("Time in milliseconds to wait between image captures.")]
     [Range(0, 5000)]
     public int RefreshMs = 100;
+    [Tooltip("Specifies what method of capture should be used.\r\n\nFile: Save to file on disk.\r\nSceneObject: Apply as texture to a new object in the scene.")]
+    public CaptureModeType CaptureMode;
 
     private PhotoCapture _photoCaptureObject;
     private GestureRecognizer _gestureRecognizer;
     private bool _photoModeStarted;
     private bool _autoCaptureStarted;
+    private List<GameObject> _gameObjects = new List<GameObject>();
     
     void Start ()
     {
@@ -74,10 +86,16 @@ public class CameraCapture : MonoBehaviour
         Debug.Log("Taking picture...");
         if (_photoModeStarted)
         {
-            var filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
-            var filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
-
-            _photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk);
+            if (CaptureMode == CaptureModeType.File)
+            {
+                var filename = string.Format(@"CapturedImage{0}_n.jpg", Time.time);
+                var filePath = System.IO.Path.Combine(Application.persistentDataPath, filename);
+                _photoCaptureObject.TakePhotoAsync(filePath, PhotoCaptureFileOutputFormat.JPG, OnCapturedPhotoToDisk); 
+            }
+            else
+            {
+                _photoCaptureObject.TakePhotoAsync(OnCapturedPhotoToMemory);
+            }
         }
     }
 
@@ -133,6 +151,42 @@ public class CameraCapture : MonoBehaviour
         {
             Debug.Log("Failed to save Photo to disk");
         }
+    }
+
+    void OnCapturedPhotoToMemory(PhotoCapture.PhotoCaptureResult result, PhotoCaptureFrame photoCaptureFrame)
+    {
+        if (result.success)
+        {
+            // Create our Texture2D for use and set the correct resolution
+            Resolution cameraResolution = PhotoCapture.SupportedResolutions.OrderByDescending((res) => res.width * res.height).First();
+            Texture2D targetTexture = new Texture2D(cameraResolution.width, cameraResolution.height);
+            // Copy the raw image data into our target texture
+            photoCaptureFrame.UploadImageDataToTexture(targetTexture);
+
+            // The frame will only have location information if we're using a LocatableCamera (your computer camera doesn't qualify)
+            Matrix4x4 cameraToWorld;
+            if (!photoCaptureFrame.TryGetCameraToWorldMatrix(out cameraToWorld))
+            {
+                Debug.Log("Unable to acquire camera to world matrix, falling back to main camera position");
+                cameraToWorld = Camera.main.cameraToWorldMatrix;
+            }
+
+            GameObject quadGameObject = GameObject.CreatePrimitive(PrimitiveType.Quad);
+            quadGameObject.transform.position = cameraToWorld.MultiplyPoint(Vector3.zero); //place the new object at the location of the camera
+            quadGameObject.transform.forward = cameraToWorld.MultiplyVector(Vector3.back); //use back so the image is facing the camera
+
+            var quadRenderer = quadGameObject.GetComponent<Renderer>();
+            if (quadRenderer != null)
+            {
+                quadRenderer.material.mainTexture = targetTexture;
+            }
+
+            _gameObjects.Add(quadGameObject);
+        }
+        
+        // Clean up
+        // TODO: Determine best pattern for enabling/disable photo mode.
+        //_photoCaptureObject.StopPhotoModeAsync(OnStoppedPhotoMode);
     }
 
     private void SetDebugText(string text)
